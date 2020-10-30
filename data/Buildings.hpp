b1f3a826461fc207b730fcebe74aa1e7a144e4f6
Mariusz Karcz <mariusz.karcz@nokia.com>
2020-10-30 20:43:44 +0100

    init

---
+++ data/Buildings.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+struct Buildings
+{
+    int metalMine = 0;
+    int crystalMine = 0;
+    int deuterExt = 0;
+    int solarGen = 0;
+    int fuzGen = 0;
+    int robots = 0;
+    int nanits = 0;
+    int shipyard = 0;
+    int metalStorage = 0;
+    int crystalStorage = 0;
+    int deuterStorage = 0;
+    int researchLab = 0;
+    int deposit = 0;
+    int silo = 0;
+    int terraformer = 0;
+};
---
+++ data/Researchs.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+struct Researchs
+{
+    int spy;
+    int comp;
+    int fight;
+    int def;
+    int shield;
+    int energy;
+    int hyperTech;
+    int combustionEngine;
+    int impusleEngine;
+    int hyperEngine;
+    int laser;
+    int ion;
+    int plasma;
+};
