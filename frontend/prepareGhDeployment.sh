61074bd8f5f2fa750cf2cf84c1e59e968c14e614
Mariusz Karcz <ujemnypozyton@gmail.com>
2021-02-23 13:21:29 +0000

    attempt to make deployment of WASM to github pages #6

--- frontend/prepareGhDeployment.sh
+++ frontend/prepareGhDeployment.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sed '1 a\"homepage\" : \"http://m-karcz.github.io/ogame-wasm-deployment\"' package.json > package.json
+sed '1 a\"homepage\" : \"http://m-karcz.github.io/ogame-wasm-deployment\",' package.json
